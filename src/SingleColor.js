7ece7397f1664f8528c36a5b0d9ae40d9a3ee777
Natalie-Cardot <nataliecardot@gmail.com>
2021-06-28 19:42:49 -0700

    Use lighter font color for shades

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -7,7 +7,10 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
   // const hex = rgbToHex(...rgb);
   // double curly braces explained here https://stackoverflow.com/a/51576729/8888320
   return (
-    <article className={`color`} style={{ backgroundColor: `rgb(${bcg})` }}>
+    <article
+      className={`color ${index > 10 && 'color-light'}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+    >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">{hexColor}</p>
     </article>
